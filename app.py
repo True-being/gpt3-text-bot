8882d5b962ac2273cda66a4495c194fecc3155b9
Truebeing <truebeing@truyou.io>
2022-12-14 16:26:26 -0700

    Main files

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, session
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio_test import ask, append_interaction_to_chat_log
+app = Flask(__name__)
+
+
+# if for some reason your conversation with TruYou gets weird, change the secret key
+app.config["SECRET_KEY"] = "TruYou-2022-Testing-Bot"
+@app.route("/twilio_test", methods=["POST"])
+
+def jabe():
+ incoming_msg = request.values["Body"]
+ chat_log = session.get("chat_log")
+ answer = ask(incoming_msg, chat_log)
+ session["chat_log"] = append_interaction_to_chat_log(incoming_msg, answer,
+ chat_log)
+ msg = MessagingResponse()
+ msg.message(answer)
+ return str(msg)
+if __name__ == "__main__":
+ app.run(debug=True)
